29a814e5b2901fa095201941b5a0951ed5cfe41d
Denilson Oliveira <juniod04@gmail.com>
2024-12-22 19:17:13 -0300

    fix: refactor crawler for oop

--- crawlers/programathor/crawler.py
+++ crawlers/programathor/crawler.py
@@ -3,19 +3,23 @@ from bs4 import BeautifulSoup as BS
 from helpers import headers, url
 
 
-response = requests.get(url, headers=headers)
+class Crawler: 
 
-page = BS(response.text, 'html.parser')
-jobs_container = page.find('div', {'class': 'col-md-9'})
-links_jobs = jobs_container.find_all('a', href=True)
-urls_jobs = []
-limit_urls = 15
+    @staticmethod
+    def get_jobs_urls():
+        response = requests.get(url, headers=headers)
 
-for link in links_jobs:
-    
-    urls_jobs.append(link['href'])
-    if len(urls_jobs) >= limit_urls:
-        print('Urls coletadas')
-        break
-   
+        page = BS(response.text, 'html.parser')
+        jobs_container = page.find('div', {'class': 'col-md-9'})
+        links_jobs = jobs_container.find_all('a', href=True)
+        urls_jobs = []
+        limit_urls = 15
+
+        for link in links_jobs:
+
+            urls_jobs.append(link['href'])
+            if len(urls_jobs) >= limit_urls:
+                print('Urls coletadas')
+                break
+        return urls_jobs
 
